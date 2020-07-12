14bf6ee199961211cd2f9b2482d8e1f38c5eac4b
psh0121 <tngus2057@naver.com>
2020-07-12 23:39:57 +0900

    앱 시작시 가장 먼저 Home 화면이 보이도록 설정 및 navigation_menu에 있는 각 메뉴들의 페이지 추가, 각 메뉴 클릭시 해당 툴바의 제목이 상황에 맞게 바뀌도록 설정

--- SmartFan/app/src/main/java/com/example/smartfan/MainActivity.java
+++ SmartFan/app/src/main/java/com/example/smartfan/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.smartfan;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -27,5 +34,18 @@ public class MainActivity extends AppCompatActivity {
 
         NavigationView navigationView = findViewById(R.id.navigationView);
         navigationView.setItemIconTintList(null);
+
+        NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        final TextView textTitle = findViewById(R.id.textTitle);
+
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+                textTitle.setText(destination.getLabel());
+            }
+        });
+
     }
 }
