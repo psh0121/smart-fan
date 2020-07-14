852c9aff31136d86a7d9b713eb9b5afc0eace770
psh0121 <tngus2057@naver.com>
2020-07-14 14:58:12 +0900

    fragment 안에 listview를 넣어 settings창에 리스트가 보이도록 설정하되 ImageView 1개와 2개의 TextView로 구성

--- SmartFan/app/src/main/java/com/example/smartfan/SettingsFragment.java
+++ SmartFan/app/src/main/java/com/example/smartfan/SettingsFragment.java
@@ -7,6 +7,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,14 +19,62 @@ import android.view.ViewGroup;
 public class SettingsFragment extends Fragment {
 
 
-    public SettingsFragment() {
-        // Required empty public constructor
-    }
+    ArrayList<Actor> actors;
+    ListView customListView;
+    private static CustomAdapter customAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        //data를 가져와서 어답터와 연결해 준다. 서버에서 가져오는게 대부분 이다.
+        actors = new ArrayList<>();
+        actors.add(new Actor("Number One", R.drawable.android, "number 1"));
+        actors.add(new Actor("Number Two", R.drawable.android, "number 2"));
+        actors.add(new Actor("Number Three", R.drawable.android, "number 3"));
+        actors.add(new Actor("Number Four", R.drawable.android, "number 4"));
+        actors.add(new Actor("Number Five", R.drawable.android, "number 5"));
+
+        customListView = (ListView) rootView.findViewById(R.id.listView_custom);
+        customAdapter = new CustomAdapter(getContext(),actors);
+        customListView.setAdapter(customAdapter);
+        customListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
+                //각 아이템을 분간 할 수 있는 position과 뷰
+                String selectedItem = (String) view.findViewById(R.id.textView_name).getTag().toString();
+                Toast.makeText(getContext(), "Clicked: " + position +" " + selectedItem, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return rootView;
+    }
+}
+
+
+//data class
+class Actor {
+    private String name;
+    private String summary;
+    private Integer thumb_url;
+
+    public Actor(String name, Integer thumb_url, String summary) {
+        this.name = name;
+        this.summary = summary;
+        this.thumb_url = thumb_url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public Integer getThumb_url() {
+        return thumb_url;
     }
 }
