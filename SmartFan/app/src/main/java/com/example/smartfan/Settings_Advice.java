3eaa358e6b31465700d8602df7a12dc3d315b845
박수현 <tngus2057@naver.com>
2020-09-06 13:22:52 +0900

    intent와 관련된 부가적인 주석 추가

--- SmartFan/app/src/main/java/com/example/smartfan/Settings_Advice.java
+++ SmartFan/app/src/main/java/com/example/smartfan/Settings_Advice.java
@@ -9,6 +9,7 @@ public class Settings_Advice extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //각 파일에 맞게 ContentView 설정
         setContentView(R.layout.settings__advice);
     }
 }
